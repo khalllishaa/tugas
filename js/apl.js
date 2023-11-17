35dd231c085062b2a944d6ad0204aba875402463
eicheich <nengindah41@gmail.com>
2023-11-17 20:55:42 +0700

    [HK] hit the api

--- js/apl.js
+++ js/apl.js
@@ -1,20 +1,64 @@
-document.addEventListener('alpine:init', () => {
-    Alpine.data('products', () => ({
-      items: [
-        {id: 1, name: 'Headphone', img: '1.jpg'", price: 20000},
-        {id: 2, name: 'Earphone', img: '2.jpg', price: 20000},
-        {id: 3, name: 'Casing', img: '3.jpeg', price: 20000},
-        {id: 4, name: 'Usb', img: '4.jpg', price: 20000},
-        {id: 5, name: 'Speaker Lamp', img: '5.jpg', price: 20000},
-      ],
-    }));
-});
+// document.addEventListener('alpine:init', () => {
+//     Alpine.data('products', () => ({
+//       items: [
+//         {id: 1, name: 'Headphone', img: '../img/products/headphone.jpg', price: 20000},
+//         {id: 2, name: 'Earphone', img: '../img/products/earphone.jpg', price: 20000},
+//         {id: 3, name: 'Casing', img: '3.jpeg', price: 20000},
+//         {id: 4, name: 'Usb', img: '4.jpg', price: 20000},
+//         {id: 5, name: 'Speaker Lamp', img: '5.jpg', price: 20000},
+//       ],
+//     }));
+// });
 
-// konverensi ke rupiah
-const rupiah = (number) => {
-    return new Intl.NumberFormat('id-ID', {
-        style: 'currency',
-        currency: 'IDR',
-        minimumFractionDigits: 0
-    }).format(number)
-}
+// // konverensi ke rupiah
+// const rupiah = (number) => {
+//     return new Intl.NumberFormat('id-ID', {
+//         style: 'currency',
+//         currency: 'IDR',
+//         minimumFractionDigits: 0
+//     }).format(number)
+// }
+document.addEventListener('DOMContentLoaded', function () {
+  // Data produk
+  const products = [
+      { id: 1, name: 'Headphone', img: './../img/products/headphone.jpg', price: 20000 },
+      { id: 2, name: 'Earphone', img: './../img/products/earphone.jpg', price: 20000 },
+      { id: 3, name: 'Casing', img: './../img/products/casing.jpeg', price: 20000 },
+      { id: 4, name: 'Usb', img: './../img/products/usb.jpg', price: 20000 },
+      { id: 5, name: 'Speaker Lamp', img: './../img/products/speaker-lamp.jpg', price: 20000 },
+  ];
+
+  // Fungsi untuk membuat elemen produk
+  function createProductElement(product) {
+      const productCard = document.createElement('div');
+      productCard.classList.add('product-card');
+
+      productCard.innerHTML = `
+          <div class="product-icons">
+              <a href="#"><i data-feather="shopping-cart"></i></a>
+              <a href="#" class="item-detail-button"><i data-feather="eye"></i></a>
+          </div>
+          <div class="product-image">
+              <img src="${product.img}" alt="${product.name}">
+          </div>
+          <div class="product-content">
+              <h3>${product.name}</h3>
+              <div class="product-price">IDR ${product.price}</div>
+          </div>
+      `;
+
+      return productCard;
+  }
+
+  // Fungsi untuk menambahkan produk ke dalam DOM
+  function renderProducts() {
+      const productContainer = document.getElementById('product-container');
+
+      products.forEach(product => {
+          const productElement = createProductElement(product);
+          productContainer.appendChild(productElement);
+      });
+  }
+
+  renderProducts(); // Memanggil fungsi untuk menampilkan produk
+});
--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');
-// ketika hamburger menu di klik
+
 document.querySelector('#hamburger-menu').onclick = () => {
   navbarNav.classList.toggle('active');
 };
@@ -17,12 +17,13 @@ document.querySelector('#search-button').onclick = (e) => {
 
 // Toggle class active untuk shopping cart
 const shoppingCart = document.querySelector('.shopping-cart');
+
 document.querySelector('#shopping-cart-button').onclick = (e) => {
   shoppingCart.classList.toggle('active');
   e.preventDefault();
 };
 
-// klik di luar elemen
+// Klik di luar elemen
 const hm = document.querySelector('#hamburger-menu');
 const sb = document.querySelector('#search-button');
 const sc = document.querySelector('#shopping-cart-button');
@@ -41,7 +42,7 @@ document.addEventListener('click', function (e) {
   }
 });
 
-// modal Box
+// Modal Box
 const itemDetailModal = document.querySelector('#item-detail-modal');
 const itemDetailButtons = document.querySelectorAll('.item-detail-button');
 
@@ -52,15 +53,100 @@ itemDetailButtons.forEach((btn) => {
   };
 });
 
-// jika klik tombol close modal
+// Jika klik tombol close modal
 document.querySelector('.modal .close-icon').onclick = (e) => {
   itemDetailModal.style.display = 'none';
   e.preventDefault();
 };
 
-// jika klik diluar modal
+// Jika klik di luar modal
 window.onclick = (e) => {
   if (e.target === itemDetailModal) {
     itemDetailModal.style.display = 'none';
   }
 };
+
+// Menyimpan produk di dalam keranjang belanja
+let shoppingCartItems = [];
+
+// Fungsi konversi ke format mata uang Rupiah
+const rupiah = (number) => {
+  return new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+    minimumFractionDigits: 0
+  }).format(number);
+};
+
+// Fungsi untuk menambah produk ke dalam keranjang belanja
+const addToCart = (product) => {
+  const existingProductIndex = shoppingCartItems.findIndex((item) => item.id === product.id);
+
+  if (existingProductIndex !== -1) {
+    shoppingCartItems[existingProductIndex].quantity++;
+  } else {
+    shoppingCartItems.push({ ...product, quantity: 1 });
+  }
+
+  renderShoppingCart();
+};
+
+// Fungsi untuk menghapus produk dari keranjang belanja
+const removeFromCart = (productId) => {
+  shoppingCartItems = shoppingCartItems.filter((item) => item.id !== productId);
+  renderShoppingCart();
+};
+
+// Fungsi untuk memperbarui tampilan keranjang belanja
+const renderShoppingCart = () => {
+  const cartContainer = document.querySelector('.cart-items');
+  cartContainer.innerHTML = '';
+
+  if (shoppingCartItems.length === 0) {
+    cartContainer.innerHTML = '<p>Keranjang belanja kosong.</p>';
+  } else {
+    shoppingCartItems.forEach((item) => {
+      const cartItem = document.createElement('div');
+      cartItem.classList.add('cart-item');
+      cartItem.innerHTML = `
+        <img src="${item.img}" alt="${item.name}" />
+        <div>
+          <h4>${item.name}</h4>
+          <p>${rupiah(item.price)} x ${item.quantity}</p>
+          <button class="remove-item" data-id="${item.id}">Remove</button>
+        </div>
+      `;
+      cartContainer.appendChild(cartItem);
+
+      const removeButtons = cartItem.querySelectorAll('.remove-item');
+      removeButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+          const productId = parseInt(button.getAttribute('data-id'));
+          removeFromCart(productId);
+        });
+      });
+    });
+  }
+
+  renderTotalPrice();
+};
+
+// Fungsi untuk menampilkan total harga belanja
+const renderTotalPrice = () => {
+  const totalPriceElement = document.querySelector('.total-price');
+  const totalPrice = shoppingCartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
+  totalPriceElement.textContent = `Total: ${rupiah(totalPrice)}`;
+};
+
+// Initialize Alpine.js data
+document.addEventListener('alpine:init', () => {
+  Alpine.data('products', () => ({
+    items: [
+      { id: 1, name: 'Headphone', img: '1.jpg', price: 20000 },
+      { id: 2, name: 'Earphone', img: '2.jpg', price: 20000 },
+      { id: 3, name: 'Casing', img: '3.jpeg', price: 20000 },
+      { id: 4, name: 'Usb', img: '4.jpg', price: 20000 },
+      { id: 5, name: 'Speaker Lamp', img: '5.jpg', price: 20000 },
+    ],
+  }));
+});
